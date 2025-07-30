7862f6b6beab2a718d195173a207b7eb8cb85538
Nicklasos <nicklasos@gmail.com>
2025-07-30 20:31:14 +0300

    Remove migrations

--- migrations/020_remove_device_id_from_code_verifications.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE code_verifications DROP COLUMN device_id;
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE code_verifications ADD COLUMN device_id VARCHAR(255);
--- +goose StatementEnd
